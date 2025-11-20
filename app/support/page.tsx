dbee3b10a82b76aee34defca740fa3d41512a033
Jude-the-wizard☠️ <sjude9928@gmail.com>
2025-11-20 04:14:42 -0800

    Update page.tsx

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -27,7 +27,7 @@ export default function SupportPage() {
 
   const handleTelegramSupport = () => {
     // Open Telegram support link in a new tab
-    const telegramUrl = "https://t.me/paygo_support001"
+    const telegramUrl = "https://t.me/Novacash8230"
     window.open(telegramUrl, "_blank")
   }
 
