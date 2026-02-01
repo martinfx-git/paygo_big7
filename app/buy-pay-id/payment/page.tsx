3d1eaa36b256bd9832fc4f34f1bd6f42344080bb
martinfx-git <martinsfx173@gmail.com>
2026-02-01 21:34:01 +0100

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -30,7 +30,7 @@ export default function PaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("1535694254")
+    navigator.clipboard.writeText("6554855586")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -89,7 +89,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">3495293423</p>
+                <p className="font-bold">6554855586</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -101,12 +101,12 @@ export default function PaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Bank Name</p>
-              <p className="font-bold">Nombank Microfinance Bank</p>
+              <p className="font-bold">Moniepoint mfb</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">Aermeans/BLESSING BENJAMINIBIA</p>
+              <p className="font-bold">Sarah Ojonny</p>
             </div>
           </div>
 
