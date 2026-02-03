ba507b2c0525ab14a08f3a02e63105646ae0d626
martinfx-git <martinsfx173@gmail.com>
2026-02-03 08:30:31 +0100

    Update Telegram support link in page.tsx

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -27,7 +27,7 @@ export default function SupportPage() {
 
   const handleTelegramSupport = () => {
     // Open Telegram support link in a new tab
-    const telegramUrl = "https://t.me/Novacash8230"
+    const telegramUrl = "t.me/Matthewxx8230"
     window.open(telegramUrl, "_blank")
   }
 
